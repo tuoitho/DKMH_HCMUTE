5d763d35e3268474acf1a9b584257d5486fe4696
Tuoi Tho da Khoc <135036590+tuoitho@users.noreply.github.com>
2024-08-05 18:42:55 +0700

    Update t.py

--- t.py
+++ t.py
@@ -1,2 +1,4 @@
 import streamlit as st
+from streamlit.components.v1 import html
+<iframe width="853" height="480" src="https://www.youtube.com/embed/Yer-bxSTxFw" title="xem trc noi dung dkmh" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
 st.title('tải ở đây https://github.com/tuoitho/dkmh2025/tree/main')
