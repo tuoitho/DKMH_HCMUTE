cb4b734b30a3b5efb43e90d9cf51ca21245be08b
Tuoi Tho da Khoc <135036590+tuoitho@users.noreply.github.com>
2024-08-07 16:04:56 +0700

    Update t.py

--- t.py
+++ t.py
@@ -2,4 +2,4 @@ import streamlit as st
 from streamlit.components.v1 import html
 st.set_page_config(layout="wide")
 st.title('tải ở đây https://github.com/tuoitho/dkmh2025/tree/main')
-html('<iframe width="853" height="480" src="https://www.youtube.com/embed/Yer-bxSTxFw" title="xem trc noi dung dkmh" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',height=1000)
+html('<iframe width="902" height="479" src="https://www.youtube.com/embed/Vju0k1wucd0" title="xem trước dkmh" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',height=1000)
