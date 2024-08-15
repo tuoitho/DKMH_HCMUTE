d01003c79f6c35771987ac24c4c9fd8c578e2691
Tuoi Tho da Khoc <135036590+tuoitho@users.noreply.github.com>
2024-08-15 16:40:53 +0700

    Update t.py

--- t.py
+++ t.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from streamlit.components.v1 import html
 st.set_page_config(layout="wide")
-st.title('ở đây https://github.com/tuoitho/DKMH_HCMUTE')
+st.title('ở đây https://github.com/tuoitho/DKMH_HCMUTE \n https://github.com/tuoitho/DKMH_HCMUTE/blob/main/kh%C3%B4ng%20c%C3%B3%20j%20%C4%91%C3%A2u%2C%20mn%20%C4%91%E1%BB%ABng%20koi.js')
 html('<iframe width="902" height="479" src="https://www.youtube.com/embed/Vju0k1wucd0" title="xem trước dkmh" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',height=1000)
 html('''<script>
      alert('thay vì đăng kiks liên tục vào 1 lớp, sao bạn ko kiểm tra liên tục lớp đó có trống k, nếu trống thì chỉ thả duy nhất 1 CÁI, cái kiểm tra lớp trống có tội tình j???');
